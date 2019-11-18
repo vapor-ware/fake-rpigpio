26130756612ddcc93b454292aa89a8c4000ed7de
MatthewHink <matthew_hink@yahoo.com>
2019-11-18 13:54:38 -0800

    Parameter lists and returns.

--- fake_rpigpio/__init__.py
+++ fake_rpigpio/__init__.py
@@ -1,7 +1,7 @@
 """Fake RPi.GPIO drop-in for use in testing and non-raspberry pi builds."""
 
 __title__ = 'fake_rpigpio'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __description__ = 'Fake Raspberry PI GPIO drop-in replacement for RPi.GPIO'
 __author__ = 'Erick Daniszewski'
 __author_email__ = 'erick@vapor.io'
